9dae466d1f27e52a6d8899ada53dcae7837969fe
Eli <elilevin11@gmail.com>
2021-12-09 17:35:19 +0200

    format

--- src/CounterVisitor.java
+++ src/CounterVisitor.java
@@ -1,36 +1,35 @@
 
-public class CounterVisitor implements IVisitor{
+public class CounterVisitor implements IVisitor {
 
     public int counter = 0;
 
-	@Override
-	public void visit(DirectoryDetails t) {
-	}
+    @Override
+    public void visit(DirectoryDetails t) {
+    }
 
-	@Override
-	public void visit(HtmlFileDetails t) {
+    @Override
+    public void visit(HtmlFileDetails t) {
         counter++;
-	}
+    }
 
-	@Override
-	public void visit(JpgFileDetails t) {
+    @Override
+    public void visit(JpgFileDetails t) {
         counter++;
-	}
+    }
 
-	@Override
-	public void visit(Mp3FileDetails t) {
+    @Override
+    public void visit(Mp3FileDetails t) {
         counter++;
-	}
+    }
 
-	@Override
-	public void visit(PptxFileDetails t) {
+    @Override
+    public void visit(PptxFileDetails t) {
         counter++;
-	}
+    }
 
-	@Override
-	public void visit(TxtFileDetails t) {
+    @Override
+    public void visit(TxtFileDetails t) {
         counter++;
-	}
+    }
 
-    
 }
